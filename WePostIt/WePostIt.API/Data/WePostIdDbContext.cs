a73dcad19cca3d575e828b0a53b1a70c8b0aa85c
MatildeGallinaEuris <matilde.gallina@euris.it>
2024-05-07 11:47:52 +0200

    overrided onmodelcreating setting message creatingtime in dbcontext

--- WePostIt/WePostIt.API/Data/WePostIdDbContext.cs
+++ WePostIt/WePostIt.API/Data/WePostIdDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.IdentityModel.Abstractions;
 using WePostIt.API.Domain;
 
 namespace WePostIt.API.Data
@@ -10,5 +12,13 @@ namespace WePostIt.API.Data
         public WePostIdDbContext(DbContextOptions options)
             : base(options) 
         { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var messages = modelBuilder.Entity<Message>();
+            messages.Property(message => message.CreationTime).IsRequired()
+                .HasDefaultValueSql("getdate()")
+                .Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
+        }
     }
 }
