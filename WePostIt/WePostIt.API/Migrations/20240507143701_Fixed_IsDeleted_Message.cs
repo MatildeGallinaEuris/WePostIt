7ac5d1b16281e6484a4b59b34a882965db57c60f
MatildeGallinaEuris <matilde.gallina@euris.it>
2024-05-07 16:39:44 +0200

    added migrations for update message entity

---
+++ WePostIt/WePostIt.API/Migrations/20240507143701_Fixed_IsDeleted_Message.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WePostIt.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixed_IsDeleted_Message : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdDeleted",
+                table: "Messages",
+                newName: "IsDeleted");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Messages",
+                newName: "IdDeleted");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
